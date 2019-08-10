340d033c7e6bb694df9bfc4c3c973ab19cba604b
Hyeong Suk Kim <whddkf2004@gmail.com>
2019-08-10 14:00:06 -0700

    Search Page returns result now, and the heading for the search page now fixed.

--- assets/javascript/search.js
+++ assets/javascript/search.js
@@ -1,3 +1,9 @@
+// Check List for Today
+// 1. Make Rating to be star format
+// 2. Show how many people voted
+
+
+
 // TMDB Api Key
 var apiKey = "4eb3939343ef4ca0932079284f76225d";
 
@@ -18,7 +24,7 @@ $(document).ready(function() {
 
 
         // Creating a relevant search query URL
-        var searchURL = "https://api.themoviedb.org/3/search/movie?api_key=" + apiKey + "&language=en-US&query=" + searchTerm + "&include_adult=true";
+        var searchURL = "https://api.themoviedb.org/3/search/movie?api_key=" + apiKey + "&language=en-US&query=" + searchTerm + "&include_adult=false";
 
         // Passing the search-term to ajax function
         var settings = {
@@ -40,24 +46,51 @@ $(document).ready(function() {
                 var tempDiv = $("<div>");
                 tempDiv.addClass("search-result-div");
         
-                var tempImage = $("<img>");
-                var imageURL = "https://image.tmdb.org/t/p/w500" + response.results[i].poster_path;
+                
+                // Make a div for image and put the image tag in it
+                var tempImgDiv = $("<div>");
+                var tempImage = $("<img id='search-result-image'>");
+                var imageURL;
+                if(response.results[i].poster_path === null) {
+                    imageURL = "./assets/images/no-image-available-icon-6.jpg"
+                } else {
+                    imageURL = "https://image.tmdb.org/t/p/w500" + response.results[i].poster_path;    
+                }
                 tempImage.attr("src", imageURL);
                 tempImage.addClass("search-result-poster");
-                
-                
+                tempImgDiv.append(tempImage);
+
+
+                // Retrieving title, release date, average vote, and vote count
+                var tempDataDiv = $("<div id='search-result-data'>");
                 var tempTitle = $("<h3>");
                 
                 // Some of the returned data does not have "original_title" data, some
                 // of them had "original_name" instead.
                 if(response.results[i].original_title !== undefined) {
-                tempTitle.text(response.results[i].original_title);
+                    tempTitle.text(response.results[i].original_title);
                 } else {
-                tempTitle.text(response.results[i].original_name);
+                    tempTitle.text(response.results[i].original_name);
                 }
+                tempTitle.addClass("search-result-title");
+
+                var tempRelease = $("<h3>");
+                tempRelease.text(response.results[i].release_date);
                 
                 
-                tempDiv.append(tempImage).append(tempTitle);
+                var tempVoteAvg = $("<h3>");
+                tempVoteAvg.text(response.results[i].vote_average);
+                
+                var tempVoteCount = $("<h3>");
+                tempVoteCount.text(response.results[i].vote_count);
+
+                
+                tempDataDiv.append(tempTitle).append(tempRelease).append(tempVoteAvg).append(tempVoteCount);
+
+                
+                
+                tempDiv.append(tempImgDiv).append(tempDataDiv);
+                
                 
                 $("#result-placehold").append(tempDiv);
 
